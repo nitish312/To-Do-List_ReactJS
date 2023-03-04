b6c80c503b0acd49580c7de004c73ecf4cea765d
Nitish <nitishchintakindi@gmail.com>
2023-03-04 16:38:33 +0530

    delete functionality added

--- src/App.js
+++ src/App.js
@@ -20,12 +20,20 @@ class App extends Component {
         }
     }
 
+    handleDelete = id => {
+        const oldItems = [...this.state.items]
+        const items = oldItems.filter((element, i) => {
+            return i != id
+        })
+        this.setState({items: items})
+    }
+
   render() {
     return (
         <div className="container-fluid my-5">
             <div className="row">
                 <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
-                    <h2 className="text-center">Today's plan heading</h2>
+                    <h2 className="text-center">Your Tasks</h2>
                     <div className="row">
                         <div className="col-9">
                             <input type="text" className="form-control" placeholder='add your tasks' value={this.state.text} onChange={this.handleChange}/>
@@ -33,12 +41,11 @@ class App extends Component {
                         <div className="col-2">
                             <button className="btn btn-warning px-5 fw-bold" onClick={this.handleAdd}>Add</button>
                         </div>
-                        <div className="container-fluid">
+                        <div className="container-fluid cls2">
                             <ul className="list-unstyle row m-5">
-                                <Plan />
                                 {
                                     this.state.items.map((value, i)=>{
-                                        return <Plan value={value}/>
+                                        return <Plan key={i} id={i} value={value} sendData={this.handleDelete}/>
                                     })
                                 }
                             </ul>
